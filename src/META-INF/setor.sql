8953ad8138be7b1996328f2d65ab78e87c3d1fa6
Mac-Swell Santos <macswell.salviano@gmail.com>
2021-02-01 22:45:16 -0300

    Ajuste da persistência

--- src/META-INF/setor.sql
+++
@@ -1,5 +0,0 @@
-insert into setor(nome) values("Compras");
-insert into setor(nome) values("Almoxarifado");
-insert into setor(nome) values("Diretoria");
-insert into setor(nome) values("Suporte Técnico");
-insert into setor(nome) values("Infraestrutura");
